622a6b279228edef86958417bf6c720ac26c6442
Sibulele Saba <Sibulele.Saba@psg.co.za>
2026-07-13 23:11:43 +0200

    Implemented edit functionality

--- src/app/modules/word-collection/edit-word/edit-word.component.ts
+++ src/app/modules/word-collection/edit-word/edit-word.component.ts
@@ -1,27 +1,75 @@
 import { Component } from '@angular/core';
 import { UpdateWordRequest } from '../../../DTOs/update-word-request';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { WordCollectionService } from '../../../services/word-collection.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { WordCollection } from '../../../models/word-collection';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-word',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './edit-word.component.html',
   styleUrl: './edit-word.component.css'
 })
 export class EditWordComponent {
-  form: any;
   wordId: number = 0;
   wordTypes: string[] = [];
+  isLoading: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private wordCollectionService: WordCollectionService, private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  form = this.formBuilder.group({
+    word: ['', Validators.required],
+    wordType: ['', Validators.required],
+  });
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (!id) {
+      this.router.navigate(['/']);
+      return;
+    }
+
+    this.wordId = Number(id);
+
+    this.loadWordTypes();
+    this.loadWord();
   }
-  
+
+  loadWordTypes(): void {
+    this.wordCollectionService.getWordTypes().subscribe({
+      next: (response) => {
+        this.wordTypes = response;
+      },
+
+      error: (error: any) => {
+        this.isLoading = false;
+        console.error('Error fetching word types:', error);
+      }
+    });
+  }
+
+  loadWord(): void {
+    this.isLoading = true;
+    this.wordCollectionService.getWordById(this.wordId).subscribe({
+      next: (response: WordCollection) => {
+        this.form.patchValue({
+          word: response.word,
+          wordType: response.wordType
+        });
+
+        this.isLoading = false;
+      },
+      error: (error: any) => {
+        this.isLoading = false;
+        console.error('Error fetching word details:', error);
+      }
+    });
+  }
+
   update(): void {
 
     if (this.form.invalid) {
@@ -29,21 +77,27 @@ export class EditWordComponent {
       return;
     }
 
+    this.isLoading = true;
+
     const request: UpdateWordRequest = {
       word: this.form.value.word!,
       wordType: this.form.value.wordType!
     };
 
-    this.wordCollectionService
-      .updateWordById(this.wordId, request)
+    this.wordCollectionService.updateWordById(this.wordId, request)
       .subscribe({
         next: () => {
+          this.isLoading = false;
           this.router.navigate(['/']);
         },
         error: (err) => {
           console.error(err);
+          this.isLoading = false;
         }
       });
+  }
 
+  cancel(): void {
+    this.router.navigate(['/']);
   }
 }
